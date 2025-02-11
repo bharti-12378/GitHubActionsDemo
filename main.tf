850c1015c36d3905d48d8246a226a63fd636edae
bharti-12378 <bharti.ghildiyal@ankercloud.com>
2025-02-11 12:59:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,16 @@
 terraform {
   backend "remote" {
-    
     organization = "Vishal-POCterraform"
 
-   
     workspaces {
       name = "DemoTerraform"
     }
   }
 }
 
-
 resource "null_resource" "example" {
   triggers = {
-    value = "A example resource that does nothing!"
+    value = "An example resource that does nothing!"
   }
 }
+
